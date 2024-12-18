bb2cf5ba58883ff031765b59fa7328bda7d85c56
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2024-12-18 07:59:49 -0500

    initial commit

---
+++ personal-finance-frontend/postcss.config.mjs
@@ -0,0 +1,10 @@
+// Purpose: PostCSS configuration for Tailwind CSS and Autoprefixer.
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
+
+export default config;
---
+++ personal-finance-frontend/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Inter, Calistoga } from "next/font/google";
+import "./globals.css";
+import { twMerge } from "tailwind-merge";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "Template Frontend",
+  description: "Frontend template for Next.js",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={twMerge(
+          inter.variable,
+          calistoga.variable,
+          "bg-[#EAEEFE] antialiased font-sans"
+        )}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
